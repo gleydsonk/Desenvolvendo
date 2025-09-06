30eedcfa6b64898e6983f0d679210e9c494438a9
Gleydson Fernandes <gleydson.kleber@gmail.com>
2025-09-06 19:25:03 +0000

    procedenciadeoperadores

---
+++ Precedenciadeoperadores.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+    int main(){
+    // ()[]              esquerda para direita
+    // ! - ++ --         direita para esquerda
+    // * / %             esquerda para direita
+    // + -               esquerda para direita
+    // < <= > >=         esquerda para direita
+    // == !=             esquerda para direita
+    // &&                esquerda para direita
+    // ||                esquerda para direita
+    // = += -= *= /= %=  direita para esquerda
+    // ,                 esquerda para direita
+
+
+
+}
