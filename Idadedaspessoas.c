c4551d508827bf01e9a67ac36598ed4824e49f4e
Gleydson Fernandes <gleydson.kleber@gmail.com>
2025-09-06 03:52:07 +0000

    idade

---
+++ Idadedaspessoas.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade1 = 25;
+    int idade2 = 60;
+
+    if (idade1 < idade2) {
+        printf("Pessoa 1 e mais nova que pessoa 2\n");
+    }
+    printf("Fora IF\n");
+
+
+
+
+}
