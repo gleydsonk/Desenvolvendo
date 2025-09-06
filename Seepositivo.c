e25f53b0cddf722cce412e6dc308c12d8693d678
Gleydson Fernandes <gleydson.kleber@gmail.com>
2025-09-06 19:01:57 +0000

    certo

---
+++ Seepositivo.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+    int main(){
+    
+    int a = -10, b = 5;
+
+    if (a > 0 || b > 0)
+    {
+        printf("Pelo menos um dos dois numeros e Positivo\n");
+    } else {
+        printf("Os dois numeros sao negativo\n");
+    }
+    printf("\n");
+    
+}
