de238b2819f41a79191fe46413d012fac3098777
Gleydson Fernandes <gleydson.kleber@gmail.com>
2025-09-06 19:36:55 +0000

    comint

--- Precedenciadeoperadores.c
+++ Precedenciadeoperadores.c
@@ -11,7 +11,21 @@
     // ||                esquerda para direita
     // = += -= *= /= %=  direita para esquerda
     // ,                 esquerda para direita
+        int a = 5;
+        int b = -10;
+        int c = 1;
 
+        // a > 0 => Verdadeiro
+        // b < 0 => Verdadeiro
+        // Verdadeiro && Verdadeiro => Verdadeiro
+        // Verdadeiro || c == 0
+        // Verdadeiro || Flso => Verdadeiro
+
+        if (a > 0 && b < 0 || c == 0) {
+            printf("A condicao e verdadeira\n");
+        } else {
+            printf("A condicao e falsa\n");
+        }
 
 
 }
