d0aa64fe96650679ae083694ce59bb4a3acb9fa2
Gleydson Fernandes <gleydson.kleber@gmail.com>
2025-09-06 04:05:15 +0000

    estoque

---
+++ Porcentagemnumeropar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int numero = 4, resultado;
+
+    resultado = numero % 2;
+
+    printf("A vareavel resultado e: %d\n", resultado);
+    
+    if(resultado % 2 == 0){
+        printf("O numero e par\n");
+
+    }
+
+
+
+
+
+}
