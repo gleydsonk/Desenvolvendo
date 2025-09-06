3cbeb6bf58c4e42183522d733989ce89eb758f94
Gleydson Fernandes <gleydson.kleber@gmail.com>
2025-09-06 19:08:28 +0000

    comint

---
+++ Seepositivo2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (){
+
+    int a = 1;
+
+    if (!a) { //(!(a > 0))
+        printf("A varivael a e zero.\n");
+        //printf("a nao e um numero positivo\n");
+    }else {
+        printf("A variavel e diferente de zero.\n");
+
+
+    }
+
+}
